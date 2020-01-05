dd848985025a30e1612afbe9fa1618f7ec4486eb
Tebinski <daniel.gteba@gmail.com>
2020-01-05 19:10:59 +0100

    Hecho! para mas info consultar kaggle

--- cat_correlation.py
+++ cat_correlation.py
@@ -2,8 +2,8 @@
 Alone in the woods (Kaggle) :
 https://www.kaggle.com/akshay22071995/alone-in-the-woods-using-theil-s-u-for-survival
 """
+#%%
 
-import os
 import math
 from collections import Counter
 import numpy as np
@@ -11,11 +11,6 @@ import seaborn as sns
 import pandas as pd
 import scipy.stats as ss
 import matplotlib.pyplot as plt
-import sklearn.preprocessing as sp
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from subprocess import check_output
 
 def conditional_entropy(x,y):
     # entropy of x given y
@@ -30,6 +25,7 @@ def conditional_entropy(x,y):
     return entropy
 
 def theil_u(x,y):
+    " compute the theils U coeficient of TWO LIST"
     s_xy = conditional_entropy(x,y)
     x_counter = Counter(x)
     total_occurrences = sum(x_counter.values())
@@ -41,6 +37,49 @@ def theil_u(x,y):
     else:
         return (s_x - s_xy) / s_x
 
-if __name__ == '__main__':
-    if not os.path.exists("/data/mushrooms.csv"):
-        exec(open("download_kaggle_files.py").read())
+#-------------------------------------------------------
+
+#%% Load files from kaggle
+exec(open("download_kaggle_files.py").read())
+df = pd.read_csv(r'./data/mushrooms.csv')
+
+#%% md
+""" Para usar Theils, tenemos que definir nuestra variable objetivo, 
+y ver si existe relacion con las otras variables.
+
+En nuestro caso la var obj es "class, que nos dice si la seta es venenosa " poisonous", 
+o comestible (edible)"""
+
+#%%
+theilu = pd.DataFrame(index=['class'],columns=df.columns)
+columns = df.columns
+for j in range(0,len(columns)):
+    u = theil_u(df['class'].tolist(),df[columns[j]].tolist())
+    theilu.loc[:,columns[j]] = u
+theilu.fillna(value=np.nan,inplace=True)
+
+#%% Representation
+tsorted = theilu.sort_values(by='class', axis=1, ascending=False, na_position='last')
+plt.figure(figsize=(20,3))
+sns.heatmap(tsorted,annot=True,fmt='.2f')
+plt.show()
+
+#%% md
+print(
+    f" Theils U nos dice que podriamos determinar al {tsorted.iloc[0,1]:.03f} "
+    f" si una seta es venenosa usando solos valores de *{tsorted.columns[1]}*.")
+
+#%% Ahora tendriamos que ver qué olores nos indican venenoso o no
+
+print(df.odor.value_counts())
+
+plt.figure()
+sns.set(rc={'figure.figsize':(15,8)})
+ax=sns.countplot(x='odor',hue='class',data=df)
+# añadimos esta parte para gestionar los nan
+for p in ax.patches:
+    patch_height = p.get_height()
+    if np.isnan(patch_height):
+        patch_height = 0
+    ax.annotate('{}'.format(int(patch_height)), (p.get_x()+0.05, patch_height+10))
+plt.show()
--- download_kaggle_files.py
+++ download_kaggle_files.py
@@ -2,13 +2,17 @@
 This code download the files of kaggle dataset
 https://www.kaggle.com/uciml/mushroom-classification
 """
-
+import os
 import kaggle
 
 if __name__ == '__main__':
-    kaggle.api.authenticate()
-    path = r'./data/'
-    kaggle.api.dataset_download_files('uciml/mushroom-classification',
-                                       path=path,
-                                       unzip=True)
-    print(f'The dataset has been downloaded at {path}')
+
+    if not os.path.exists("./data/mushrooms.csv"):
+        kaggle.api.authenticate()
+        path = r'./data/'
+        kaggle.api.dataset_download_files('uciml/mushroom-classification',
+                                           path=path,
+                                           unzip=True)
+        print(f'The dataset has been downloaded at {path}')
+    else:
+        print('Data is already downloaded')
